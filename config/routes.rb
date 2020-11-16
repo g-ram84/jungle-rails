73bb950ff09d71c53186747c6570c563c794fa17
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-16 13:07:26 -0800

    Commit time

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,13 @@ Rails.application.routes.draw do
   # Example of regular route:
     get 'about' => 'about#show'
 
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
