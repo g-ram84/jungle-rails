92810ee6684be607ff8a3fa3d9dd84848a1101b3
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-17 13:14:06 -0800

    More tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "Is created with a password and password_confirmation" do
+      new_user = User.create(password: "abcdefg", password_confirmation: "abcdefg")
+      expect(new_user.persisted?).to be(true)
+    end
+    it "Password and password_confirmation match" do
+      new_user = User.create(password: "abcdefg", password_confirmation: "abcdefg")
+      expect(new_user.password).to be(new_user.password_confirmation)
+    end
+    it "Is created with a name, and email" do
+      new_user = User.create(first_name: "Graeme", last_name: "Chalmers", email:"chalmers.graeme@gmail.com", password: "abcdefg", password_confirmation: "abcdefgh")
+      expect(new_user.persisted?).to be(true)
+    end
+  end
 end
